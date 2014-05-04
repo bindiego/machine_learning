a6fa7b3a5d97218d288fe4e58a7c457c4684b916
Bin Wu <bindiego@outlook.com>
2014-05-05 01:19:04 +0800

    Ex6 done.

--- week7/mlclass-ex6-005/mlclass-ex6/dataset3Params.m
+++ week7/mlclass-ex6-005/mlclass-ex6/dataset3Params.m
@@ -23,11 +23,22 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+min_err = inf;
 
+for curr_c = [0.01 0.03 0.1 0.3 1 3 10 30]
+    for curr_sigma = [0.01 0.03 0.1 0.3 1 3 10 30]
+        model = svmTrain(X, y, curr_c, @(x1, x2) gaussianKernel(x1, x2, curr_sigma));
 
+        predictions = svmPredict(model, Xval);
+        prediction_err = mean(double(predictions ~= yval));
 
-
-
+        if prediction_err < min_err
+            min_err = prediction_err;
+            C = curr_c;
+            sigma = curr_sigma;
+        end % end if
+    end % end for curr_sigma
+end % end for curr_c
 
 % =========================================================================
 
--- week7/mlclass-ex6-005/mlclass-ex6/processEmail.m
+++ week7/mlclass-ex6-005/mlclass-ex6/processEmail.m
@@ -97,14 +97,11 @@ while ~isempty(email_contents)
     %       str2). It will return 1 only if the two strings are equivalent.
     %
 
+    [member, index] = ismember(str, vocabList);
 
-
-
-
-
-
-
-
+    if member > 0
+        word_indices = [word_indices; index];
+    end
 
     % =============================================================
 
